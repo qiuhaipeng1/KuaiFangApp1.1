aa111470edd35b94af8a21157fd7a0b2d8ee18c6
asus-PC <18519776740@163.com>
2016-11-16 19:32:58 +0800

    aaa

--- app/src/main/java/com/kuaifang/fragment/MyFragment.java
+++ app/src/main/java/com/kuaifang/fragment/MyFragment.java
@@ -20,6 +20,7 @@ public class MyFragment extends Fragment {
     private int picResId;
     private ImageView view;
 
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         // 从宿主中获得传过来的值（图片的资源的id）
